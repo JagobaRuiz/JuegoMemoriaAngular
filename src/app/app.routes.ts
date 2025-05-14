ae8fb785d3c088d9cb91511f15d866e6e4cd753a
JagobaRuiz <jagoba.ruizab@elorrieta-errekamari.com>
2025-05-14 12:43:35 +0200

    Corregir error animaciones al subir a GitHub Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import {InicioComponent} from '../components/inicio/inicio.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-  { path: 'inicio', component: InicioComponent },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent  },
 ];
